784de063a914021709161f770593ffa65fff9c7c
Johnny Squardo <johnnysquardo@gmail.com>
2022-09-09 17:08:33 -0400

    Add Logging

--- ProjectApi/Program.cs
+++ ProjectApi/Program.cs
@@ -1,13 +1,25 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Logger
+var logger = new LoggerConfiguration().ReadFrom
+    .Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
